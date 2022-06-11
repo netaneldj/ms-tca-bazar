1475057501f9f75b3e64e5ac457229be1abb505f
Netanel David Jamilis <23279713+netaneldj@users.noreply.github.com>
2022-06-11 20:16:58 -0300

    get producto & cliente given id venta

--- bazar/src/main/java/com/todocodeacademy/bazar/service/VentaService.java
+++ bazar/src/main/java/com/todocodeacademy/bazar/service/VentaService.java
@@ -38,14 +38,10 @@ public class VentaService implements IVentaService{
         venta.setFecha_venta(LocalDate.now());
         venta.setTotal(calculateTotalProductos(venta.getListaProductos()));
         updateStock(venta.getListaProductos());
-        Cliente c = clienteService.findCliente(venta.getUnCliente().getId_cliente());
-        venta.setUnCliente(c);
-        for(Producto p: venta.getListaProductos()){
-            Producto producto = productoService.findProducto(p.getCodigo_producto());
-            p.setCosto(producto.getCosto());
-            p.setMarca(producto.getMarca());
-            p.setCantidad_disponible(producto.getCantidad_disponible());
-            p.setNombre(producto.getNombre());
+        venta.setUnCliente(clienteService.findCliente(venta.getUnCliente().getId_cliente()));
+        for(int i=0;  i < venta.getListaProductos().size(); i++){
+            venta.getListaProductos().set(i,
+                    productoService.findProducto(venta.getListaProductos().get(i).getCodigo_producto()));
         }
         repository.save(venta);
     }
